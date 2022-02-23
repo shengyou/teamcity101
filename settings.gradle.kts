8a484d4ff7ba6aece67ba45392a64f8e5556b8b2
Shengyou Fan <shengyoufan@gmail.com>
2022-02-23 15:42:59 +0800

    setup gradle build

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,12 @@
-
 rootProject.name = "teamcity101"
 
+plugins {
+    `gradle-enterprise`
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+    }
+}
